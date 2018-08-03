c9ccfb3b3104e96f6845a93f9e96a7935e92faa2
kewang <cpckewang@gmail.com>
2018-08-03 15:57:07 +0800

    Test Counter

---
+++ src/main/java/tw/kewang/Counter.java
@@ -0,0 +1,31 @@
+package tw.kewang;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.shared.SharedCount;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.security.SecureRandom;
+
+public class Counter {
+    private static final String PATH = "/caches";
+    private static final String CONNECT_STRING = "127.0.0.1";
+    private static final String NAMESPACE = "test";
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString(CONNECT_STRING)
+                .sessionTimeoutMs(5000).connectionTimeoutMs(5000).retryPolicy(retryPolicy)
+                .namespace(NAMESPACE).build();
+
+        client.start();
+
+        SharedCount count = new SharedCount(client, PATH, 0);
+
+        count.start();
+
+        count.setCount(new SecureRandom().nextInt());
+    }
+}
